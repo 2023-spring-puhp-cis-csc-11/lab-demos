ebe0e06b56ca0937c3a6319c0b69a9466111690f
Professor P <mike-ubuntu-vbox@lenomike.home>
2023-03-13 05:34:00 -0700

    Finishing touches on Lab 02

--- 02 - Hybrid Programs/driver.cpp
+++ 02 - Hybrid Programs/driver.cpp
@@ -2,23 +2,26 @@
 #include <iostream>
 using std::cout, std::endl;
 
-extern "C"
-{
-	long hello();
+extern "C" {
+	signed long hello();
+	void sea();
 }
 
 int main()
 {
 	cout << "My name is Chaplain Mondover. Welcome to my driver program!" << endl;
-	
 	cout << "I will now call the hello module ..." << endl;
 	
-	long helloResult = hello();
+	signed long helloResult = hello();
 	
 	cout << "The driver is now back in control." << endl;
-	cout << "The driver received the following value: " << helloResult << endl;	
+	cout << "The driver received the following value: " << helloResult << endl;
+	
 	cout << "Goodbye!" << endl;
+	
+	sea();
 	
 	return 0;
 }
+
 
--- 02 - Hybrid Programs/hello.asm
+++ 02 - Hybrid Programs/hello.asm
@@ -11,6 +11,13 @@ section .data
 
 ;;;
 ; CStrings
+
+; We could if we wanted, but not convenient
+SOME_BYTES						db		123,178,239,23
+SOME_QWORDS						dq		12300000000,1780000000,23900000,2300000
+; We could if we wanted, but not convenient
+
+
 WELCOME_MSG						db		"Hello, this is the hello module!"
 WELCOME_MSG_LEN					equ		$-WELCOME_MSG
 
@@ -20,7 +27,7 @@ INPUT_MSG_LEN					equ		$-INPUT_MSG
 GOODBYE_MSG						db		"Thank you for your input. Your number will be returned to the driver."
 GOODBYE_MSG_LEN					equ		$-GOODBYE_MSG
 
-CRLF							db		13,10
+CRLF							db		13,10   ; \r\n
 CRLF_LEN						equ		$-CRLF
 
 ;;;
@@ -37,7 +44,7 @@ FD_STDOUT						equ		1
 ; BSS Section (uninitialized data)
 section .bss
 
-INPUTTED_INT			resq	1
+INPUTTED_LONG			resq	1
 
 ;;;;;;;;;;;;;;
 ; Text Section
@@ -47,10 +54,10 @@ section .text
 ;;;;;;;;;;;;;;;;;;;;;;
 ; Our external symbols
 extern libPuhfessorP_inputSignedInteger64
-extern libPuhfessorP_printSignedInteger64
+;extern libPuhfessorP_printSignedInteger64
 
 
-; Our entry point
+; Our hello function
 global hello
 hello:
 	
@@ -72,7 +79,7 @@ hello:
 	; Utilize libP to input an integer from the user
 	; It's using the signature: long libPuhfessorP_printSignedInteger64()
 	call libPuhfessorP_inputSignedInteger64
-	mov [INPUTTED_INT], rax
+	mov [INPUTTED_LONG], rax
 	
 	; Say thanks
 	mov rax, SYS_WRITE					; System call code
@@ -83,8 +90,9 @@ hello:
 	call crlf
 	
 	; We're done
-	mov rax, [INPUTTED_INT]	; Mov our return code into rax
-	ret						; Return control back to the caller (driver)
+	mov rax, [INPUTTED_LONG]			; Mov our return code into rax
+	ret									; Return control back to the caller (driver)
+
 
 ;;;
 ; Custom function to print a CRLF
@@ -98,7 +106,6 @@ crlf:
 	syscall
 	
 	ret
-
 
 
 
